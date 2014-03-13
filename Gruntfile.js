57b1a51bfeaf21a4e0cf7333218352c0ab99c829
Steven Bassett <steven.j.bassett@gmail.com>
2014-03-13 14:07:25 -0700

    Updated something here....

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,8 @@ module.exports = function (grunt) {
     // load all grunt tasks matching the `grunt-*` pattern
     require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({});
+    grunt.initConfig({
+        ""
+    });
     grunt.registerTask('default', []);
 }
